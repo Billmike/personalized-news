3aeded3e8352a3de8a0d97a7b7593cae9384abd7
Billmike <ayelegun.kayode@gmail.com>
2024-11-17 18:03:17 +0100

    chore: setup heroku postgres

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,14 +1,14 @@
 import { NextResponse } from 'next/server';
-import { supabase } from '@/app/lib/supabase';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient()
 
 export async function GET() {
   try {
-    const { data, error } = await supabase.from('User').select('*') as never;
-
-    if (error) throw error;
+    const users = await prisma.user.findMany();
 
     return NextResponse.json({
-      data,
+      users,
     });
   } catch (error) {
     console.error('Error fetching users:', error);
